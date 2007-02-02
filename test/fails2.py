d28cb62d1085823e61d580c4a1edb5541b5c534e
steder <steder@c0a01155-9b19-0410-a6ce-5b585efae7cc>
2007-02-02 14:13:23 +0000

    Initial version of run-test.py and some example scripts to verify that it does, in fact, seem to recognize the difference between failing MPI scripts.  I'm hoping this will serve as the basis for a Parallel PyUnit implementation that I'll implement next.
    A    test/ssizecheck.py
    D    test/run-all.sh
    A    test/fails.py
    A    test/README
    A    test/fails2.py
    M    test/run-test.py
    
    
    git-svn-id: https://maroonmpi.googlecode.com/svn/trunk@18 c0a01155-9b19-0410-a6ce-5b585efae7cc

---
+++ test/fails2.py
@@ -0,0 +1,32 @@
+import mpi
+import sys, Numeric
+import os
+
+print "Creating Data Array..."
+data = Numeric.array( [1,2,3,4], Numeric.Int32 )
+
+rank, size = mpi.init( len(sys.argv), sys.argv )
+print "(%s,%s): initialized..." %(rank,size)
+
+returnCode = 1
+
+if ( rank == 0 ):
+    print "(%s,%s): sending: %s" %( rank, size, data )
+    request = mpi.isend( data, 4, mpi.MPI_INT, 0, 0, mpi.MPI_COMM_WORLD )
+    print "(%s,%s): request#: %s" %( rank, size, request )
+    print "(%s,%s): receiving..." %(rank,size)
+    data2 = mpi.recv( 4, mpi.MPI_INT, 0, 0, mpi.MPI_COMM_WORLD )
+    print "(%s,%s): received: %s" %(rank, size, data2)
+
+    file = open("fails.py.out","a")
+    file.write( "My return code is %s\n"%returnCode )
+    file.close()
+    mpi.barrier(mpi.MPI_COMM_WORLD)
+    
+    #mpi.abort(mpi.MPI_COMM_WORLD, returnCode)
+    mpi.init()
+else:
+    mpi.barrier(mpi.MPI_COMM_WORLD)
+    
+
+mpi.finalize()
--- test/run-test.py
+++ test/run-test.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
-# A simple python script to mpiexec
-# an MMPI test script.
+# A simple python script to mpiexec a parallel python script
 
-import sys, os
-
+import sys, os, traceback, string
 
 try:
     numprocs = int(sys.argv[1])
     script = os.path.realpath(sys.argv[2])
+
     mode = os.P_WAIT
-    #exe = "/usr/bin/mpiexec"
-    exe = "/usr/bin/python"
-    #args = "-l","-n",numprocs,"python",script
-    args = (script)
-    returncode = os.spawnv( mode, exe, args )
-    print returncode
+    exe = os.popen( "which mpiexec" ).readline().strip("\n")
+    python = os.popen( "which python" ).readline().strip("\n")
+    args = ("-l", "-n", "%s"%(numprocs),"%s"%(python),  "%s"%(script),)
+    command = "mpiexec -l -n %s python %s" % (numprocs, script)
 
+    print exe,string.join(args," ")
+    
+    #result = os.popen( command ).read()
+
+    #result = os.spawnv( mode, exe, args )
+
+    #exe = os.popen( "which touch" ).readline().strip("\n")
+    #args = ("spawnv.out",)
+
+    result = os.spawnv( os.P_WAIT, exe, (exe,)+args )
+    print result
+    
 except:
+    traceback.print_exc()
     print "Usage:  run.py number_of_processors parallel_script.py"
-    
-    
-
-
