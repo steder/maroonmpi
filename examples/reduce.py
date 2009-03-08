a16286f5568b95e95a64f62fbe5e4fc8bf08a066
steder <steder@c0a01155-9b19-0410-a6ce-5b585efae7cc>
2009-03-08 05:03:05 +0000

    Added print statements for debugging purposes to the reduce example
    git-svn-id: https://maroonmpi.googlecode.com/svn/trunk@40 c0a01155-9b19-0410-a6ce-5b585efae7cc

--- examples/reduce.py
+++ examples/reduce.py
@@ -2,4 +2,7 @@ import sys,mpi
 rank,size = mpi.init(len(sys.argv),sys.argv)
 sigma = mpi.reduce( 7, 1, mpi.MPI_INT, mpi.MPI_SUM, 0, mpi.MPI_COMM_WORLD )
 print "Sum:",sigma 
+print "MPI_INT:", mpi.MPI_INT
+print "MPI_SUM:", mpi.MPI_SUM
+print "MPI_COMM_WORLD:", mpi.MPI_COMM_WORLD
 mpi.finalize()
